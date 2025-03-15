cc33e613fc632f2e27e9537905c7b6f3d09ec321
Chowdary-Hari <chandrahari321@outlook.com>
2025-03-15 08:31:32 +0530

    automated commit #942

--- code.sh
+++ code.sh
@@ -1,38 +1,72 @@
 #!/bin/bash
 
-# only works on centos8 aarch64
+# Ensure script runs as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
 
-# install mongodb
-# sudo yum install dnf -y
-# curl -s https://raw.githubusercontent.com/ChaitanyaChandra/DevOps/main/2.ANSIBLE/roles/mongodb/files/mongo.repo > /etc/yum.repos.d/mongodb-org-6.0.repo
-# dnf --disablerepo=AppStream install -y mongodb-org
-# sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-# systemctl restart mongod
+echo "Starting setup..."
+###################################################################################
+##################################     mongodb    ##################################
+###################################################################################
+
+# Install DNF package manager (if not already installed)
+sudo yum install dnf -y
+
+# Copy the MongoDB repository configuration file to the YUM repository directory
+cp mongodb-org-7.0.repo /etc/yum.repos.d/mongodb-org-7.0.repo
+
+# Install MongoDB while disabling the AppStream repository to avoid conflicts
+dnf --disablerepo=AppStream install -y mongodb-org
+
+# Update MongoDB configuration to allow external connections by replacing 127.0.0.1 with 0.0.0.0
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+# Restart MongoDB service to apply the changes
+systemctl restart mongod
 
 
-# setup user 
+###################################################################################
+##################################     nodejs    ##################################
+###################################################################################
+# setup user
 adduser spec
 
 curl -sL https://rpm.nodesource.com/setup_16.x | bash -
 yum install nodejs -y 
 
 yum install git -y 
+# shellcheck disable=SC2164
 cd /home/spec/
-git clone https://github.com/ChaitanyaChandra/app.git
+git clone https://github.com/OpsInfinity/app.git
+# shellcheck disable=SC2164
 cd app/
 cat package.sh | bash
 
-db_user=chaitu
-db_pass=123Chaitu
+# Configure MongoDB environment variables
+db_user="prasad"
+db_pass="123Prasad"
 
-echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
+# mongodb end point or connection string
+
+echo "Setting up environment variables..."
+echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.3zmmc.mongodb.net/login-app-db?retryWrites=true&w=majority" >> /home/spec/app/files/spec.service
+
+
 cp files/spec.service /etc/systemd/system/
-systemctl start spec
+systemctl restart spec
+
+###################################################################################
+##################################     nginx    ##################################
+###################################################################################
 
 sudo yum install epel-release -y
 sudo yum install nginx -y 
 
+# shellcheck disable=SC2216
 yes | cp -rf files/nginx.conf /etc/nginx/nginx.conf
+# shellcheck disable=SC2216
 yes | cp -rf files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
 setenforce 0
 systemctl restart nginx
